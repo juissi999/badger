cb95beab1e657f9fa1aeaa2633f290efc688d33a
juissi999 <jussi.kaatiala@gmail.com>
2020-06-28 17:15:31 +0300

    Fix github name on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-col class="text-center" cols="12">
     {{ new Date().getFullYear() }} -
-    <a href="http://github.com/juissi999">jussi999</a>
+    <a href="http://github.com/juissi999">juissi999</a>
   </v-col>
 </template>
