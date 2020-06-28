1f338f5b96bc794d07f79b8f0216e72702a1f729
juissi999 <jussi.kaatiala@gmail.com>
2020-06-28 22:07:08 +0300

    Update chart-page texts

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -3,6 +3,9 @@
     <v-row class="text-center">
       <v-col class="mb-3 ma-md-4 pa-0 pa-md-5">
         <h1 class="display-2 font-weight-bold mb-1 mb-md-3">Chart</h1>
+        <p class="subheading font-weight-regular mb-0 mb-md-3">
+          Displays a sum chart of ticks (y-axis) against time (x-axis).
+        </p>
       </v-col>
     </v-row>
     <v-card class="mx-auto" max-width="600">
@@ -24,7 +27,7 @@ export default {
         labels: this.$store.state.count.map(el => new Date(el)),
         datasets: [
           {
-            label: 'Clients',
+            label: 'Ticks',
             borderColor: '#8e5ea2',
             data: this.$store.state.count.map(el => {
               allCount++
